786e6a607e791eeb0bbc9117eefd08481f6901d2
shubhranshutripathi9 <shubhranshutripathi0fficial-email.com>
2026-06-07 17:14:23 +0530

    the parameters for various distributions determined

---
+++ src/fitting.py
@@ -0,0 +1,44 @@
+from scipy import stats
+import numpy as np
+
+
+def fit_distributions(log_returns):
+    results = {}
+
+    # fit to normal
+    mu, sigma = stats.norm.fit(log_returns)
+    # mu and sigma are mean and sd
+    results["norm"] = {"mu": mu, "sigma": sigma}
+
+    # fit to students t
+    df, loc, scale = stats.t.fit(log_returns)
+    # df is deg of freedom, loc means location(axis shift) and scale is the scale parameter
+    results["students t"] = {"df": df, "loc": loc, "scale": scale}
+
+    # fit to skew-normal
+    a, loc, scale = stats.skewnorm.fit(log_returns)
+    # a is shape like alpha in chi^2, rest same
+    results["skewnorm"] = {"a": a, "loc": loc, "scale": scale}
+
+    # fit to chi-squared
+    df, loc, scale = stats.chi2.fit(log_returns)
+    results["chi2"] = {"df": df, "loc": loc, "scale": scale}
+
+    # fit to erlang
+    a, loc, scale = stats.erlang.fit(log_returns)
+    results["erlang"] = {"a": a, "loc": loc, "scale": scale}
+
+    # fit to gamma
+    a, loc, scale = stats.gamma.fit(log_returns)
+    results["gamma"] = {"a": a, "loc": loc, "scale": scale}
+
+    # fit to logarithmic
+    p, loc, scale = stats.logser.fit(log_returns)
+    results["Chi2"] = {"p": p, "loc": loc, "scale": scale}
+
+    # fit to generalised logistic
+    c, loc, scale = stats.genlogistic.fit(log_returns)
+    results["genlogistic"] = {"c": c, "loc": loc, "scale": scale}
+
+    # all adjusted, the parameters are returned as a dictionary
+    return results
